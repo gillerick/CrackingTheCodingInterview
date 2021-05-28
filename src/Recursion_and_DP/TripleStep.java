ad4309e7c6f6466e229abaec5b06e1e7ac511598
gillerick <=>
2021-05-28 16:10:23 +0300

    Added memoization solution

--- src/Recursion_and_DP/TripleStep.java
+++ src/Recursion_and_DP/TripleStep.java
@@ -5,7 +5,10 @@ package Recursion_and_DP;
 * Implement a method to count how many possible ways the child can run up the stairs
 * */
 
+import java.util.Arrays;
+
 public class TripleStep {
+    //Recursive solution
     public int countWays(int n){
         if (n < 0){
             return 0;
@@ -16,8 +19,29 @@ public class TripleStep {
         }
     }
 
+    //Memoization solution
+    public int _countWays(int n){
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return _countWays(n,memo);
+    }
+
+    public int _countWays(int n, int[] memo){
+        if (n < 0){
+            return 0;
+        }else if (n == 0){
+            return 1;
+        }else if (memo[n] > -1) {
+            return memo[n];
+        }else{
+            memo[n] = _countWays(n-1, memo) + _countWays(n-2, memo) + _countWays(n-3, memo);
+
+            return memo[n];
+        }
+    }
+
     public static void main(String[] args) {
         TripleStep steps = new TripleStep();
-        System.out.println(steps.countWays(7));
+        System.out.println(steps._countWays(7));
     }
 }
